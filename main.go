ae2090f3f87fc1f81c33457e9bde4f697a5e19ea
tasnia-shahrin <tasniashahrin269@gmail.com>
2025-04-11 11:36:03 +0600

    ghum ashe

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ func main() {
 	fmt.Println("Hello world") //Println er ln=line.ekta line e print korbe then enter diye dibe
 
 	// variable declaration and initialization
-	a := 10  //variable gula RAM e store hoy
+	a := 10       //variable gula RAM e store hoy
 	var x int = 5 //var name data_type
-	var y=6.79 //data-type na lekhleo go bujhe ney
-	b:=true
-	b=false //first e kono value declaration er time e colon dite hoy.next e oi var e value assign korte gele colon dite hobe na
-	const p="tasnia"
+	var y = 6.79  //data-type na lekhleo go bujhe ney
+	b := true
+	b = false //first e kono value declaration er time e colon dite hoy.next e oi var e value assign korte gele colon dite hobe na
+	const p = "tasnia"
 	//p="orin"
 	fmt.Println(a)
 	fmt.Println(x)
@@ -21,14 +21,25 @@ func main() {
 	fmt.Println(p)
 
 	// if-else
-	age:=24
-	
-	if age>18{
+	age := 24
+
+	if age > 18 {
 		fmt.Println("you're eligible to be married")
-	}else if age<18 {
+	} else if age < 18 {
 		fmt.Println("you're not eligible to be married but you can love someone")
-	}else{
+	} else {
 		fmt.Println("you're not eligible to be married. you are just a teenager")
 	}
 	// operator: >,<,>=,<=,==,&&,||,!
+
+	// switch-case
+	switch a {
+	case 5:
+		fmt.Println("a is 5")
+	case 10, 15:
+		fmt.Println("a is either 10 or 15")
+	default:
+		fmt.Println("a is neither 5 nor 10 or 15")
+	}
+
 }
