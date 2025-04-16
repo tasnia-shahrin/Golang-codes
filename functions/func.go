a26649bdd32a8cd6ad333cf669b1360b8a9e5b5a
tasnia-shahrin <tasniashahrin269@gmail.com>
2025-04-17 01:23:27 +0600

    another commit

--- functions/func.go
+++ functions/func.go
@@ -5,7 +5,7 @@ import "fmt"
 var a=10
 
 //standard or named function : that has name
-func add(x int,y int){
+func add(x int,y int){ //parameter : x,y--> value receive
 	sum:=x+y
 	fmt.Println(sum)
 }
@@ -19,6 +19,20 @@ func init(){
 }
 
 func main(){
-	add(2,4)
+	add(2,4) //argument:2,4 =>value pass
 	fmt.Println(a)
+
+	// anonymous function:Jar name thake na
+	func(a int,b int){
+		c:=a+b
+		fmt.Println(c)
+	}(5,7) //go te anonymous function rekhe deya jayna.sthe sthe call/execute  korte hoy.etakei bole Immediately Invoked Function Expression(IIFE)
+
+	//Function expression or Assign function in variable: jekono func/anonymous function e variable er modde assign korai holo function expression
+	sub:=func(a int,b int){
+		sub:=a-b
+		fmt.Println(sub)
+	}
+	sub(20,15)
+	
 }
