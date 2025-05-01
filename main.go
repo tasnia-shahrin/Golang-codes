4a1efeeaa66eaa3b68dc1e2db40068504a2ec408
tasnia-shahrin <tasniashahrin269@gmail.com>
2025-05-01 19:06:23 +0600

    slice

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main //main go file er package hobe main
 
-import(
+import (
 	"fmt"
 	"tasnia/mathlib"
-)  //go er built-in package(fmt=format)
+
+	
+) //go er built-in package(fmt=format)
 
 var v=10 //global scope e v:=10 lekhle error dekhabe
 
@@ -20,6 +22,14 @@ type User struct{
 	Age int
 	FavFoods []int
 }
+
+//Variadic function:unlimited amount argument pass korte parbo
+func printSlice(numbers ... int){
+	fmt.Println(numbers)
+	fmt.Println(len(numbers))
+	fmt.Println(cap(numbers))
+}
+
 
 func main() {
 
@@ -112,5 +122,35 @@ func main() {
 	fmt.Println(*struct_addr)
 	fmt.Println(struct_addr.Name)
 
-}
 
+	//Slice:
+	arr3:=[6]string{"this","is","a","go","interview","question"}
+	fmt.Println(arr3)
+
+	slice:=arr3[1:4] //slice from array
+	fmt.Println(slice) //["is","a","go"]
+
+	slice2:=slice[1:2] //slice from slice
+	fmt.Println(slice2)
+
+	s1:=[3]int{1,2,5} //array
+	fmt.Println(s1)
+	s2:=[]int{1,2,5} //slice literal
+	fmt.Println(s2,len(s2),cap(s2))
+	//slice using make()
+	s3:=make([]int,3) //3=length
+	s3[0]=5
+	fmt.Println(s3,len(s3),cap(s3)) //[5,0,0] len=3,cap=3
+	s4:=make([]int,3,5)//5=capacity
+	s4[2]=10 //[5]
+	fmt.Println(s4,len(s4),cap(s4))
+	
+	var slice3 []int //empty or nil slice:
+	fmt.Println(slice3)
+	slice3=append(slice3, 1)
+	fmt.Println(slice3)
+	//slice underlying array rule:1024B porjonto=>double(100%) increase hobe size.1024 par hoye gele 25% kore barbe.
+
+	//variadic func:
+	printSlice(3,3,4,4,6,565,332)
+}
